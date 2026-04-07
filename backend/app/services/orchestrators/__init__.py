301c5fa7004414a582b96ae05ffaff61b1f6c557
snake <snake1042552642@gmail.com>
2026-04-07 16:02:24 +0800

    删除多余残留文件

--- backend/app/services/orchestrators/__init__.py
+++
@@ -1,5 +0,0 @@
-from ..agents.implementations.orchestrators.base import BaseOrchestrator
-from ..agents.implementations.orchestrators.conversation import ConversationOrchestrator
-from ..agents.implementations.orchestrators.notes import NotesOrchestrator
-
-__all__ = ["BaseOrchestrator", "ConversationOrchestrator", "NotesOrchestrator"]
